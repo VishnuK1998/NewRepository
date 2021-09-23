4db860b1167d8f95a3920376c175596ff77637b0
VishnuK1998 <91088994+VishnuK1998@users.noreply.github.com>
2021-09-23 16:40:38 +0530

    Add comments to gacha.py

--- src/gacha.py
+++ src/gacha.py
@@ -1,10 +1,11 @@
-# Gacha modeling
-# P5 = 1.0%
-# P4 = 20.%
+# Gacha modeling program
+# Assumes a simple model without any form of pity where pulls can be 5*, 4* or 3*
+# Probability of pulling a 5* = 1.0%
+# Probability of pulling a 4* = 20.0%
 
 from random import random
 
-
+# Function to simulate gacha pull
 def gachapull (P):
     x = random()
     if (x<=P[0]): J = 0
@@ -18,12 +19,13 @@ N = int(input("Enter the number of pulls: "))
 #No. of characters pulled
 L = [0,0,0]
 
-#Base probabilities
+#Base cumulative probabilities
 P = [0,0,0]
 P[0] = 0.01
 P[1] = 0.20 + P[0]
 P[2] = 1
 
+#Counting the results
 for i in range(0,N):
     y = gachapull(P)
     L[y] += 1
