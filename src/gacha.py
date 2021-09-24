fe7dd47dd795abcc4cecc943dd6c187f915134c3
VishnuK1998 <91088994+VishnuK1998@users.noreply.github.com>
2021-09-24 16:48:52 +0530

    Update gacha.py
    Improved functionality of gacha.py. Added classes

--- src/gacha.py
+++ src/gacha.py
@@ -3,31 +3,48 @@
 # Probability of pulling a 5* = 1.0%
 # Probability of pulling a 4* = 20.0%
 
-from random import random
+from random import random, choice
+import numpy as np
+import pandas as pd
 
-# Function to simulate gacha pull
-def gachapull (P):
-    x = random()
-    if (x<=P[0]): J = 0
-    elif (x<=P[1]): J = 1
-    else: J = 2
-    return J
+class Gacha:
 
+    def __init__(self):
+        pity = 0
+        P = []
+        CL = []
+        
+
+    def pull(self):
+        x = random()
+        if (x<=self.P[0]): y = 0
+        elif (x<=self.P[1]): y = 1
+        else: y = 2
+        N = choice(self.CL[y])
+        self.pity = 0 if y==0 else self.pity + 1
+        if self.pity>50: 
+            self.P[0] += 0.02
+            self.P[1] += 0.02
+        return y, N
+
+def fetchlist():
+    df = pd.read_csv('charlist.csv')
+    split = list(df.groupby("Rarity"))
+    CL = []
+    for x in split[::-1]:
+        CL.append(list((x[1])["CHName"]))
+    return(CL)
+
+
+StdG = Gacha()
+StdG.P = [0.01, 0.21, 1]
+StdG.CL = fetchlist()
+StdG.pity = 0
+
+count = np.zeros(3)
 
 N = int(input("Enter the number of pulls: "))
-
-# No. of characters pulled
-L = [0,0,0]
-
-# Base cumulative probabilities
-P = [0,0,0]
-P[0] = 0.01
-P[1] = 0.20 + P[0]
-P[2] = 1
-
-# Counting the results
 for i in range(0,N):
-    y = gachapull(P)
-    L[y] += 1
-    
-print("Pull results: {}".format(L))    
+    y,name = StdG.pull()
+    print(5-y,'\t',name)
+      
